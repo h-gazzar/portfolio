190547bf253793e739f9361b40c6b31232813601
h-gazzar <h.elgazzar81@gmail.com>
2025-12-06 14:24:26 +0200

    initial commit

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <h1>Welcome to My React App</h1>
+            <p>This is a basic React application setup.</p>
+        </div>
+    );
+};
+
+export default App;
